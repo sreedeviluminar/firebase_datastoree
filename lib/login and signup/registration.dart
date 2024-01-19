7d59fc7da7a374f6e85179641268588f8c4a0258
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-01-19 14:44:56 +0530

    initial commit

--- lib/login and signup/registration.dart
+++ lib/login and signup/registration.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_datastoree/login%20and%20signup/login.dart';
 import 'package:flutter/material.dart';
 
-class Registration extends StatelessWidget {
+class Registration extends StatefulWidget {
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  var formkey = GlobalKey<FormState>();
+  String? pass;// to collect value from pwd field
+  var pwdcontroller = TextEditingController();
+  var is_pwd_hidden = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,45 +22,94 @@ class Registration extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    hintText: "Email"),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    hintText: "Password"),
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    hintText: "Confirm Password"),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.purple),
-                  onPressed: () {},
-                  child: const Text("Register Here",
-                      style: TextStyle(color: Colors.white))),
-            ],
+          child: Form(
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      hintText: "Email"),
+                  validator: (email) {
+                    if (email!.isEmpty || !email.contains('@')) {
+                      return "invalid email/ or empty field";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: pwdcontroller,
+                  validator: (pwd) {
+                    pass = pwd;
+                    if (pwd!.isEmpty || pwd.length < 6) {
+                      return "length should be > 6 / field must not be empty";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if(is_pwd_hidden == true){
+                                is_pwd_hidden = false;
+                              }else{
+                                is_pwd_hidden = true;
+                              }
+                            });
+                          },
+                          icon: Icon(is_pwd_hidden == true
+                              ? Icons.visibility_off
+                              : Icons.visibility)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      hintText: "Password"),
+                  obscureText: is_pwd_hidden,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (cpwd) {
+                    //if(cpwd != pwdcontroller.text)
+                    if (cpwd != pass) {
+                      return "password must be same";
+                    } else {
+                      return null;
+                    }
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      hintText: "Confirm Password"),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple),
+                    onPressed: () {
+                      var valid = formkey.currentState?.validate();
+                      if (valid == true) {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Login()));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("Please check the fields")));
+                      }
+                    },
+                    child: const Text("Register Here",
+                        style: TextStyle(color: Colors.white))),
+              ],
+            ),
           ),
         ),
       ),
